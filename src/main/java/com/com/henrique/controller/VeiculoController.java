6669057b3446c36b30baedec768f99ef6444089b
Henrique <henriquevergara@hotmail.com>
2019-12-30 11:50:59 -0300

    Criacao Dao

--- src/main/java/com/com/henrique/controller/VeiculoController.java
+++ src/main/java/com/com/henrique/controller/VeiculoController.java
@@ -1,21 +1,24 @@
 package com.com.henrique.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.com.henrique.dao.veiculoDao;
 import com.com.henrique.model.Veiculo;
 
 @Controller
 public class VeiculoController {
-
 	
+	veiculoDao dao = new veiculoDao();
+
 	@RequestMapping(value="/cadastro",method = RequestMethod.POST)
 	public String salvar(Veiculo veiculo) {
 		
-		System.out.println(veiculo.getAnoModelo() + veiculo.getMarca());
-		return "redirect:/cadastro";
+		dao.salvar(veiculo);
+		return "redirect:/lista";
 	}
 	
 	@RequestMapping("/cadastro")
@@ -26,4 +29,19 @@ public class VeiculoController {
 		
 	}
 	
+	@RequestMapping("/lista")
+	public String buscaTodos() {
+		
+		dao.buscaTodos();
+		
+		return "redirect:/cadastro";
+	}
+	
+	@RequestMapping("/lista/{placa}")
+	public String buscaPorPlaca(@PathVariable String placa) {
+		
+		System.out.println(dao.buscar(placa));
+		
+		return "redirect:/cadastro";
+	}
 }
---
+++ src/main/java/com/com/henrique/dao/veiculoDao.java
@@ -0,0 +1,42 @@
+package com.com.henrique.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.com.henrique.model.Veiculo;
+
+public class veiculoDao {
+
+	
+	List<Veiculo> listaVeiculos = new ArrayList<>();
+	
+	public void salvar(Veiculo veiculo) {
+		
+		this.listaVeiculos.add(veiculo);
+		
+	}
+	
+	public String buscar(String placa) {
+		
+		for (Veiculo veiculo : this.listaVeiculos) {
+		
+			if (veiculo.getPlaca().equals(placa)) {
+				return "Veiculo encontrado na base de dados";
+			}
+		}
+		return "Veiculo nao encontrado";
+	}
+	
+	public void buscaTodos(){
+		if (listaVeiculos.size() > 0) {			
+			int i = 1;
+			for (Veiculo veiculo : listaVeiculos) {
+				System.out.println(i+ ": " + veiculo.getMarca() + " " + veiculo.getModelo() + " " + veiculo.getPlaca() + " " + veiculo.getAnoModelo());
+				i++;
+			}
+		}else {
+			System.out.println("Nenhum veiculo cadastrado");
+		}
+	}
+	
+}
